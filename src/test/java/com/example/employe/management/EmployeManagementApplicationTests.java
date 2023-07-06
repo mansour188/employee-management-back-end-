808f0a540993dbfde565b0277ccd21173cc2e4b1
mansour <mansourbenahmed89@gmail.com>
2023-07-06 12:04:31 +0100

    test pipeline

---
+++ src/test/java/com/example/employe/management/EmployeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employe.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
